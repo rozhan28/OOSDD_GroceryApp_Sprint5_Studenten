bfc932eb47ce70f4755ee81c141ce20cb0356316
rozhan28 <rozhanarya1@gmail.com>
2025-10-08 19:41:37 +0200

    UC14 prijzen toegevoegd

--- Grocery.Core/Models/Product.cs
+++ Grocery.Core/Models/Product.cs
@@ -1,22 +1,41 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Diagnostics;
 
 namespace Grocery.Core.Models
 {
     public partial class Product : Model
     {
         [ObservableProperty]
-        public int stock;
-        public DateOnly ShelfLife { get; set; }
-        public Product(int id, string name, int stock) : this(id, name, stock, default) { }
+        private int stock;
 
-        public Product(int id, string name, int stock, DateOnly shelfLife) : base(id, name) 
+        [ObservableProperty]
+        private decimal price; 
+
+        public DateOnly ShelfLife { get; set; }
+
+        // Constructor zonder THT
+        public Product(int id, string name, int stock, decimal price)
+            : this(id, name, stock, price, default)
+        {
+        }
+
+        // Constructor met THT
+        public Product(int id, string name, int stock, decimal price, DateOnly shelfLife)
+            : base(id, name)
         {
             Stock = stock;
+            Price = price;
             ShelfLife = shelfLife;
         }
+
+        public Product(int id, string name, int stock)
+            : this(id, name, stock, 0m, default)
+        {
+        }
+
         public override string? ToString()
         {
-            return $"{Name} - {Stock} op voorraad";
+            return $"{Name} - {Stock} op voorraad - €{Price:F2}";
         }
     }
 }
