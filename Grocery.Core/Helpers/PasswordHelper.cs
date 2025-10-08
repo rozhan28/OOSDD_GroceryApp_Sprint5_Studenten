422e53f1a1089659c31ef16143aeff6c54b1eb1d
rozhan28 <rozhanarya1@gmail.com>
2025-10-08 18:58:19 +0200

    fixed passwordhelpers

--- Grocery.Core/Helpers/PasswordHelper.cs
+++ Grocery.Core/Helpers/PasswordHelper.cs
@@ -8,20 +8,45 @@ namespace Grocery.Core.Helpers
         public static string HashPassword(string password)
         {
             byte[] salt = RandomNumberGenerator.GetBytes(16);
-            var hash = Rfc2898DeriveBytes.Pbkdf2(Encoding.UTF8.GetBytes(password), salt, 100000, HashAlgorithmName.SHA256, 32);
+
+            var hash = Rfc2898DeriveBytes.Pbkdf2(
+                Encoding.UTF8.GetBytes(password),
+                salt,
+                100000,
+                HashAlgorithmName.SHA256,
+                32);
+
             return Convert.ToBase64String(salt) + "." + Convert.ToBase64String(hash);
         }
 
         public static bool VerifyPassword(string password, string storedHash)
         {
-            var parts = storedHash.Split('.');
+            if (string.IsNullOrEmpty(password) || string.IsNullOrWhiteSpace(storedHash))
+                return false;
+
+            var parts = storedHash.Split('.', 2);
             if (parts.Length != 2) return false;
 
-            var salt = Convert.FromBase64String(parts[0]);
-            var hash = Convert.FromBase64String(parts[1]);
-            var inputHash = Rfc2898DeriveBytes.Pbkdf2(Encoding.UTF8.GetBytes(password), salt, 100000, HashAlgorithmName.SHA256, 32);
+            try
+            {
+                var salt = Convert.FromBase64String(parts[0]);
+                var hash = Convert.FromBase64String(parts[1]);
 
-            return CryptographicOperations.FixedTimeEquals(inputHash, hash);
+                var inputHash = Rfc2898DeriveBytes.Pbkdf2(
+                    Encoding.UTF8.GetBytes(password),
+                    salt,
+                    100000,
+                    HashAlgorithmName.SHA256,
+                    32);
+
+                return CryptographicOperations.FixedTimeEquals(inputHash, hash);
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
         }
     }
 }
+
+
